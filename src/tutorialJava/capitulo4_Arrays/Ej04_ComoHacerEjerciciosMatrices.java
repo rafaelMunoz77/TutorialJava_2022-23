e370fe41c12306faeae6e7d73d947dc6595f9f19
Rafael Muñoz <rafael.munoz77@gmail.com>
2022-11-14 09:35:24 +0100

    feat (chapter 4): ej 4 - how to work with matrixes

--- src/tutorialJava/capitulo4_Arrays/Ej04_ComoHacerEjerciciosMatrices.java
+++ src/tutorialJava/capitulo4_Arrays/Ej04_ComoHacerEjerciciosMatrices.java
@@ -12,18 +12,70 @@ public class Ej04_ComoHacerEjerciciosMatrices {
 	 */
 	public static void main(String[] args) {
 		int a[] = new int[] {1, 2, 3, 4, 5};
-		int m[][] = new int[][] { 	{1, 	0, 		0, 		0, 		0}, 
-									{0, 	6, 		0, 		0, 		0}, 
-									{0,		0, 		13, 	0, 		0}, 
-									{0, 	0, 		0, 		19, 	0}, 
-									{0, 	0, 		9, 		0, 		25} };
+		int m[][] = new int[][] { 	{1, 	2, 		3, 		4, 		0}, 
+									{5, 	6, 		0, 		8, 		9}, 
+									{10,	0, 		12, 	13, 	14}, 
+									{0, 	16,		17, 	18, 	19}, 
+									{20, 	21, 	22, 	0, 		24} };
 //		int m[][] = new int[5][5];
 		
 //		inicializaMatriz(m);
 		imprimeMatriz(m);
+		System.out.println("Es dispersa: " + esmatrizDispersa(m));
 	}
 	
 
+	public static boolean esmatrizDispersa (int m[][]) {
+		boolean dispersa = true;
+		for (int i = 0; i < m.length; i++) {
+			boolean hayCerosEnEstaFila = false;
+			for (int j = 0; j < m[i].length; j++) {
+				if (m[i][j] == 0) {
+					hayCerosEnEstaFila = true;
+				}
+			}
+			// Acabo de recorrer una fila
+			if (hayCerosEnEstaFila == false) {
+				dispersa = false;
+			}
+		}
+		return dispersa;
+	}
+	
+	
+	
+	public static boolean esmatrizDispersa2 (int m[][]) {
+		// Recorro las filas buscando una que no tenga ceros
+		for (int i = 0; i < m.length; i++) {
+			boolean hayCerosEnEstaFila = false;
+			for (int j = 0; j < m[i].length; j++) {
+				// Si encuentro un elemento que sea cero lo registro
+				if (m[i][j] == 0) { hayCerosEnEstaFila = true; }
+			}
+			// Acabo de recorrer una fila
+			if (!hayCerosEnEstaFila) {
+				return false;
+			}
+		}
+		
+		// Recorro las columnas buscando una que no tenga ceros
+		for (int i = 0; i < m[0].length; i++) {
+			boolean hayCerosEnEstaColumna = false;
+			for (int j = 0; j < m.length; j++) {
+				// Si encuentro un elemento que sea cero lo registro
+				if (m[i][j] == 0) { hayCerosEnEstaColumna = true; }
+			}
+			// Acabo de recorrer una fila
+			if (!hayCerosEnEstaColumna) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	
 	public static boolean esMatrizPositiva (int n[][]) {
 		for (int i = 0; i < n.length; i++) {
 			for (int j = 0; j < n[i].length; j++) {
